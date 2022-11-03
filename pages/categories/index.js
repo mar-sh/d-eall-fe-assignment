4db06685d1ca7045992acb3bb7168cfd182c4fe4
mar-sh <marasi.af@gmail.com>
2022-11-03 09:55:27 +0700

    use getServerSideProps for categories

--- pages/categories/index.js
+++ pages/categories/index.js
@@ -28,7 +28,7 @@ function CategoriesPage() {
   );
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
