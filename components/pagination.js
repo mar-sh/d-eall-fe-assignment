01318c4300e713da72162aa90702b8a2bda1987f
mar-sh <marasi.af@gmail.com>
2022-11-03 09:05:14 +0700

    extract classes for pagination button

--- components/pagination.js
+++ components/pagination.js
@@ -6,7 +6,7 @@ function Pagination({ onNextPage, onPreviousPage, currentPage, disabled }) {
       <button
         onClick={onPreviousPage}
         disabled={disabled || Number(currentPage) - 1 < 1}
-        className="inline-flex items-center py-2 px-4 mr-3 text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white disabled:cursor-not-allowed"
+        className="button-pagination"
       >
         <svg
           aria-hidden="true"
@@ -28,7 +28,7 @@ function Pagination({ onNextPage, onPreviousPage, currentPage, disabled }) {
       <button
         onClick={onNextPage}
         disabled={disabled}
-        className="inline-flex items-center py-2 px-4 text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white disabled:cursor-not-allowed"
+        className="button-pagination"
       >
         Next
         <svg
